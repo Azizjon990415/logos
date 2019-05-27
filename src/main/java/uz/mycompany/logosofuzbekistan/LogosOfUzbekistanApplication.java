b9f5a14aa8dc14a3297dbd8afbeff54ffd3dfcb5
Azizjon <azizjon990415@gmail.com>
2019-05-27 19:45:53 +0500

    projectCreated

---
+++ src/main/java/uz/mycompany/logosofuzbekistan/LogosOfUzbekistanApplication.java
@@ -0,0 +1,13 @@
+package uz.mycompany.logosofuzbekistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogosOfUzbekistanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogosOfUzbekistanApplication.class, args);
+    }
+
+}
